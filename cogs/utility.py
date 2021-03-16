e6a1339ccbb06192635cbdc7544f5e22796bbb71
vNziie <74474942+vNziie@users.noreply.github.com>
2021-03-16 08:59:07 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -665,7 +665,7 @@ class Utility(commands.Cog):
     async def ping(self, ctx):
         """Pong! Returns your websocket latency."""
         embed = discord.Embed(
-            title="Pong! Websocket Latency:",
+            title="🏓 Pong! Websocket Latency:",
             description=f"{self.bot.ws.latency * 1000:.4f} ms",
             color=self.bot.main_color,
         )
